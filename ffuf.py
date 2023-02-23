571f5749434edd83d154ff89357aad1149dd8425
melforze <denis_office101421@mail.ru>
2023-02-23 21:41:11 +0300

    last update

--- ffuf.py
+++ ffuf.py
@@ -8,13 +8,13 @@ def ffuf(f_data):
         scope_h = scope.replace(".", "_")
         if port == '443':
             subprocess.check_output(
-                ["ffuf", "-recursion", "-recursion-depth", "3", "-c", "-w", "/Users/ustindenis/Desktop/work/wordlists/fuzz.txt:FUZZ", "-u", f"https://{scope}/FUZZ", "-rate", f"{rate}", "-o", f"{scope_h}.html", "-of", "html", "&"])
+                ["ffuf", "-recursion", "-recursion-depth", "3", "-c", "-w", f"{path_to_wordlist}:FUZZ", "-u", f"https://{scope}/FUZZ", "-rate", f"{rate}", "-o", f"{scope_h}.html", "-of", "html", "&"])
         elif port == '80':
             subprocess.check_output(
-                ["ffuf", "-recursion", "-recursion-depth", "3", "-c", "-w", "/Users/ustindenis/Desktop/work/wordlists/fuzz.txt:FUZZ", "-u", f"http://{scope}/FUZZ", "-rate", f"{rate}", "-o", f"{scope_h}.html", "-of", "html", "&"])
+                ["ffuf", "-recursion", "-recursion-depth", "3", "-c", "-w", f"{path_to_wordlist}:FUZZ", "-u", f"http://{scope}/FUZZ", "-rate", f"{rate}", "-o", f"{scope_h}.html", "-of", "html", "&"])
         else:
             subprocess.check_output(
-                ["ffuf", "-recursion", "-recursion-depth", "3", "-c", "-w", "/Users/ustindenis/Desktop/work/wordlists/fuzz.txt:FUZZ", "-u", f"http://{scope}:{port}/FUZZ", "-rate", f"{rate}", "-o", f"{scope_h}.html", "-of", "html", "&"])
+                ["ffuf", "-recursion", "-recursion-depth", "3", "-c", "-w", f"{path_to_wordlist}:FUZZ", "-u", f"http://{scope}:{port}/FUZZ", "-rate", f"{rate}", "-o", f"{scope_h}.html", "-of", "html", "&"])
         print(f"{scope} already done!")
     except:
         print("ffuf broke or not installed")
